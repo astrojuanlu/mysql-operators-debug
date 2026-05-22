9dff26f5e14f4afaa0c39f7f94415ef3b803ea21
Sinclert Pérez <sinclert.perez@canonical.com>
2026-05-22 15:19:53 +0200

    [K8s] Define TLS private-key configs + tests

--- kubernetes/src/charm.py
+++ kubernetes/src/charm.py
@@ -10,7 +10,6 @@ from ops.main import main
 if is_wrong_architecture() and __name__ == "__main__":
     main(WrongArchitectureWarningCharm)
 
-import json
 import logging
 import random
 from time import sleep
@@ -565,8 +564,10 @@ class MySQLOperatorCharm(MySQLCharmBase, TypedCharmBase[CharmConfig]):
             logger.info("Skipping group replication restart")
             return OperationResult.RETRY_RELEASE
 
-        ongoing_ops = [relation.data[unit].get("operations", "[]") for unit in self.peers.units]
-        ongoing_ops = [json.loads(op) for op in ongoing_ops]
+        ongoing_ops = [
+            self.rolling_ops.is_waiting_callback("replication", unit.name)
+            for unit in self.peers.units
+        ]
 
         if self.is_unit_primary and any(ongoing_ops):
             logger.info("Skipping group replication restart")
